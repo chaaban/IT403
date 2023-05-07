144daca59536e5cf2cb16b7d9ce4344a258b7228
Mouhamad Triki <a190120@students.gu.edu.lb>
2023-05-07 12:43:10 +0300

    project

--- project/Mouhamad.Triki/project_Weather.sh
+++ project/Mouhamad.Triki/project_Weather.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace YOUR_API_KEY with your own OpenWeatherMap API key
-API_KEY=1d16bb055372bbf5e8239eabc91117cd
+API_KEY=2962dd9221930175241256d4305697aa
 
 # Prompt the user to enter a city name and country code
 echo "Enter city name: "
@@ -9,19 +9,32 @@ read city
 echo "Enter country code (e.g. US): "
 read country
 
+# Prompt the user to choose a temperature unit
+echo "Enter temperature unit (C or F): "
+read unit
+
 # Use curl to get the weather data for the specified location
 url="https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}"
 weather_data=$(curl -s "$url")
 
-# Parse the JSON response to get the current temperature and weather description
-temp_k=$(echo "$weather_data" | jq -r '.main.temp')
+# Parse the JSON response to get the current weather data
+temp=$(echo "$weather_data" | jq -r '.main.temp')
+if [ "$unit" == "F" ]; then
+  temp=$(printf "%.0f" "$(echo "scale=2; ($temp - 273.15) * 9/5 + 32" | bc)")
+  unit="°F"
+else
+  temp=$(printf "%.0f" "$(echo "scale=2; $temp - 273.15" | bc)")
+  unit="°C"
+fi
 desc=$(echo "$weather_data" | jq -r '.weather[0].description')
-
-# Convert temperature to Fahrenheit and Celsius
-temp_f=$(echo "scale=2; ($temp_k - 273.15) * 9/5 + 32" | bc)
-temp_c=$(echo "scale=2; ($temp_k - 273.15)" | bc)
+humidity=$(echo "$weather_data" | jq -r '.main.humidity')
+wind_speed=$(echo "$weather_data" | jq -r '.wind.speed')
+pressure=$(echo "$weather_data" | jq -r '.main.pressure')
 
 # Print the results to the user
-echo "Current weather conditions in ${city}, ${country}:"
-echo "Temperature: ${temp_f}°F / ${temp_c}°C"
-echo "Weather description: ${desc}"
+echo "Current weather in ${city}, ${country}:"
+echo "  Temperature: ${temp}${unit}"
+echo "  Weather description: ${desc}"
+echo "  Humidity: ${humidity}%"
+echo "  Wind speed: ${wind_speed} m/s"
+echo "  Pressure: ${pressure} hPa"
