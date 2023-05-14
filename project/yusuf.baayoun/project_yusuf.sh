061b3cc68be7448829ad3284524810ac07b4a85d
Yousef Baayoun <a220027@students.gu.edu.lb>
2023-05-14 12:03:06 +0300

    Test

--- project/yusuf.baayoun/project_yusuf.sh
+++ project/yusuf.baayoun/project_yusuf.sh
@@ -3,11 +3,31 @@
 # Set time interval to refresh every 5 seconds
 INTERVAL=5
 
-# Initialize total data usage to 0
+# Initialize data usage to 0
 TOTAL_MB=0
+START_TOTAL=0
+END_MB=0
 
 # Store start time
 START_TIME=$(date +"%s")
+
+ # Get network interfaces
+  INTERFACES1=$(ifconfig -s | cut -d ' ' -f 1 | sed '1d')
+
+# Check each interface
+ for INTERFACE in $INTERFACES1; do
+   # Skip the loopback interface
+    if [ $INTERFACE == "lo" ]; then
+      continue
+    fi
+
+    # Get the amount of data at the begining
+    START_DATA=$(ifconfig $INTERFACE | grep -i "rx pac" | awk '{print $5}')
+    # Get data in MB
+    START_MB=$((START_DATA/1000000))
+    #Get total data from all interfaces
+    START_TOTAL=$((START_TOTAL + START_MB))
+done
 
 # Function to handle Ctrl+C
 function ctrl_c() {
@@ -18,7 +38,9 @@ function ctrl_c() {
   # Calculate minutes and seconds
   MINUTES=$((ELAPSED_TIME / 60))
   SECONDS=$((ELAPSED_TIME % 60))
-
+  
+  #Calculate total usage
+  TOTAL_MB=$((END_MB - START_TOTAL))
   # Print the total data usage and elapsed time
   echo "Total usage in $MINUTES minute(s) , $SECONDS seconds is: $TOTAL_MB MB"
   exit 0
@@ -30,7 +52,8 @@ trap ctrl_c INT
 while true; do
   # Get the current time
   TIME=$(date +"%T")
-
+  # Initialize total end data usage to 0
+  END_TOTAL=0
   # Get network interfaces
   INTERFACES=$(ifconfig -s | cut -d ' ' -f 1 | sed '1d')
 
@@ -47,8 +70,8 @@ while true; do
     # Get data in MB
     MB=$((DATA/1000000))
 
-    # Accumulate total data usage
-    TOTAL_MB=$((TOTAL_MB + MB))
+    #Get total end data from all interfaces
+    END_TOTAL=$((END_TOTAL + MB))
 
     # Print the time and the data used by each interface
     echo "$TIME Interface $INTERFACE usage: $MB MB"
@@ -56,4 +79,8 @@ while true; do
 
   # Wait for the specified interval
   sleep $INTERVAL
+  
+  #Capture final data usage from all interfaces
+  END_MB=$END_TOTAL
+
 done
