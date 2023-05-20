ab8a4072d6b50dc78e1842637a6909391757c395
SirinKerkatli <a200063@students.gu.edu.lb>
2023-05-20 13:41:13 +0300

    project

---
+++ project/sirin.kerkatli/project.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+count=0
+numberOfWords=0
+
+read -p "Press Enter to Start: " start
+
+start=$(date +%M:%S)
+echo $start
+
+startSeconds=$(echo $start | cut -d : -f2)
+startMinutes=$(echo $start | cut -d : -f1)
+
+end_time=$((SECONDS + 60))
+
+while [ $SECONDS -lt $end_time ];
+do
+	let numberOfWords=numberOfWords+1
+	
+	line=$(shuf -n 1 /usr/share/dict/words)
+	echo $line
+
+	echo "Enter your input:"
+	read -t $end_time word
+	if [ $? -eq 0 ]
+		then
+		
+			if [ "$word" == $line ]
+				then
+					let count=$count+1
+					echo $count
+				fi
+		else
+			break
+		fi
+done
+	
+end=$(date +%M:%S)
+echo $end
+
+endSeconds=$(echo $end | cut -d : -f2)
+endMinutes=$(echo $end | cut -d : -f1)
+
+timeSeconds=$((10#$endSeconds - 10#$startSeconds))
+timeMinutes=$((10#$endMinutes - 10#$startMinutes))
+
+if [ $timeSeconds -lt 0 ]
+	then
+		time="$((timeMinutes - 1))":"$((60 + timeSeconds))"
+		
+	else
+		time="$timeMinutes":"$timeSeconds"
+	fi
+
+finalSeconds=$(echo $time | cut -d : -f2)
+echo "seconds: $finalSeconds"
+
+finalMinutes=$(echo $time | cut -d : -f1)
+echo "minutes: $finalMinutes"
+
+totalSeconds=$(((10#$finalMinutes*60)+10#$finalSeconds))
+echo "total seconds: $totalSeconds"
+
+echo "you typed $numberOfWords words, $count of them is correct, in $totalSeconds seconds"
+
+speed=$(((10#$numberOfWords*60)/10#$totalSeconds))
+
+echo "your speed is $speed words per minute"
